4e4422db7b443848c63d3f3849dafd40e9f8c224
Lascelles <waunaneville@gmail.com>
2026-05-17 13:33:08 +0300

    feat: add PlantsPage component with plant data fetching, normalization, and search state management

--- src/components/PlantPage.jsx
+++ src/components/PlantPage.jsx
@@ -3,7 +3,7 @@ import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
-function PlantPage() {
+function PlantsPage() {
   const [plants, setPlants] = useState([]);
   const [searchQuery, setSearchQuery] = useState("");
 
@@ -19,48 +19,4 @@ function PlantPage() {
       });
   }, []);
 
-  const handleAddPlant = (newPlant) => {
-    fetch("http://localhost:6001/plants", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(newPlant),
-    })
-      .then((resp) => resp.json())
-      .then((createdPlant) => {
-        setPlants((previousPlants) => [
-          ...previousPlants,
-          { ...createdPlant, soldOut: false },
-        ]);
-      });
-  };
-
-  const handleToggleSoldOut = (plantId) => {
-    setPlants((previousPlants) =>
-      previousPlants.map((plant) =>
-        plant.id === plantId
-          ? { ...plant, soldOut: !plant.soldOut }
-          : plant
-      )
-    );
-  };
-
-  const handleSearchChange = (event) => {
-    setSearchQuery(event.target.value);
-  };
-
-  const filteredPlants = plants.filter((plant) =>
-    plant.name.toLowerCase().includes(searchQuery.toLowerCase())
-  );
-
-  return (
-    <main>
-      <NewPlantForm onAddPlant={handleAddPlant} />
-      <Search searchQuery={searchQuery} onSearchChange={handleSearchChange} />
-      <PlantList plants={filteredPlants} onToggleSoldOut={handleToggleSoldOut} />
-    </main>
-  );
-}
-
-export default PlantPage;
+  
